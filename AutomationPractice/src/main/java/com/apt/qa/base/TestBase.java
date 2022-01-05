d10c105bb723b646f770364c7ee1f82fdd8fc5c7
Tabrez <Tabrez@DESKTOP-2EFCOPA>
2022-01-05 18:03:49 +0530

    sdd

--- AutomationPractice/src/main/java/com/apt/qa/base/TestBase.java
+++ AutomationPractice/src/main/java/com/apt/qa/base/TestBase.java
@@ -41,7 +41,7 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tabrez\\.jenkins\\workspace\\APT\\driver.exe");	
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tabrez\\.jenkins\\workspace\\APT\\driver\\chromedriver.exe");	
 			driver = new ChromeDriver(); 
 		}
 		
